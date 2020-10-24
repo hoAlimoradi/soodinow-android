73a2220ba52111f690bcf24001e91537fee79238
omid <naghipoor.omid@gmail.com>
2020-10-24 12:23:09 +0330

    version name and app logo added to splash

--- presentation/src/main/java/com/paya/presentation/ui/splash/SplashActivity.kt
+++ presentation/src/main/java/com/paya/presentation/ui/splash/SplashActivity.kt
@@ -1,23 +1,40 @@
 package com.paya.presentation.ui.splash
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.paya.presentation.MainActivity
 import com.paya.presentation.R
+import kotlinx.android.synthetic.main.activity_splash.*
 import kotlinx.coroutines.delay
+
 
 class SplashActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_splash)
 		
+		setVersionName()
+		
 		lifecycleScope.launchWhenStarted {
-		//	delay(3000)
-			startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+			delay(3000)
+			startActivity(Intent(this@SplashActivity,MainActivity::class.java))
 			finish()
 		}
 		
+	}
+	
+	private fun setVersionName() {
+		try {
+			val pInfo: PackageInfo =
+				packageManager.getPackageInfo(packageName,0)
+			val version = pInfo.versionName
+			versionTxt.text = version
+		} catch (e: PackageManager.NameNotFoundException) {
+			e.printStackTrace()
+		}
 	}
 }
