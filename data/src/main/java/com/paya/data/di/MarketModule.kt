e242d6f31df25023cc9cdbf696c585d5a1f5f638
macbook <arsalan.asanbar@gmail.com>
2020-10-31 19:51:59 +0330

    fix return type

--- data/src/main/java/com/paya/data/di/MarketModule.kt
+++ data/src/main/java/com/paya/data/di/MarketModule.kt
@@ -2,17 +2,11 @@ package com.paya.data.di
 
 import com.paya.common.Mapper
 import com.paya.data.mapper.*
-import com.paya.data.repository.AccountRepositoryImpl
 import com.paya.data.repository.MarketRepositoryImpl
-import com.paya.domain.models.remote.AccountCardRemoteModel
 import com.paya.domain.models.remote.MarketRemoteModel
-import com.paya.domain.models.remote.RegisterRemoteModel
 import com.paya.domain.models.repo.*
-import com.paya.domain.repository.AccountRepository
 import com.paya.domain.repository.MarketRepository
 import com.paya.domain.tools.UseCase
-import com.paya.domain.usecase.account.GetAccountUseCase
-import com.paya.domain.usecase.auth.*
 import com.paya.domain.usecase.market.MarketSmallListUseCase
 import dagger.Binds
 import dagger.Module
@@ -24,7 +18,7 @@ import dagger.hilt.android.components.ActivityComponent
 abstract class MarketModule {
 	
 	@Binds
-	abstract fun bindGetMarketSmallListMapperRemoteRepo(mapper: AccountCardRemoteRepoMapper): Mapper<
+	abstract fun bindGetMarketSmallListMapperRemoteRepo(mapper: MarketRemoteRepoMapper): Mapper<
 			MarketRemoteModel,
 			MarketRepoModel
 			>
