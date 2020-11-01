a2b83ab8d278222992371ff9b75b5aedbc697bd4
omid <naghipoor.omid@gmail.com>
2020-11-01 18:21:22 +0330

    DataBinding added to MarketFragment

--- presentation/src/main/java/com/paya/presentation/ui/market/MarketFragment.kt
+++ presentation/src/main/java/com/paya/presentation/ui/market/MarketFragment.kt
@@ -5,16 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.paya.presentation.R
+import com.paya.presentation.databinding.FragmentMarketBinding
 
 class MarketFragment : Fragment() {
+	
+	private lateinit var mBinding: FragmentMarketBinding
 	
 	override fun onCreateView(
 		inflater: LayoutInflater,container: ViewGroup?,
 		savedInstanceState: Bundle?
 	): View? {
 		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_market,container,false)
+		mBinding = DataBindingUtil.inflate(
+			inflater,
+			R.layout.fragment_market,
+			container,
+			false
+		)
+		return mBinding.root
 	}
 	
 }
