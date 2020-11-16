7e55dee9161d485998f046ab3580ef428ff833e1
omid <naghipoor.omid@gmail.com>
2020-11-16 16:56:27 +0330

    implement create low risk fragment

---
+++ presentation/src/main/java/com/paya/presentation/ui/createLowRiskAccount/CreateLowRiskAccountFragment.kt
@@ -0,0 +1,30 @@
+package com.paya.presentation.ui.createLowRiskAccount
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.paya.presentation.R
+import com.paya.presentation.databinding.FragmentCreateLowRiskAccountBinding
+
+class CreateLowRiskAccountFragment : Fragment() {
+	
+	private lateinit var mBinding: FragmentCreateLowRiskAccountBinding
+	
+	override fun onCreateView(
+		inflater: LayoutInflater,container: ViewGroup?,
+		savedInstanceState: Bundle?
+	): View? {
+		// Inflate the layout for this fragment
+		mBinding = DataBindingUtil.inflate(
+			inflater,
+			R.layout.fragment_create_low_risk_account,
+			container,
+			false
+		)
+		return mBinding.root
+	}
+	
+}
