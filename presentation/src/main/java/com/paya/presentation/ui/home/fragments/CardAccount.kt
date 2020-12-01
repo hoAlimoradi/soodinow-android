0561cf81fa06638f531a76b1890077bfcbedd4c8
omid <naghipoor.omid@gmail.com>
2020-12-01 10:45:47 +0330

    fix some bugs profile

--- presentation/src/main/java/com/paya/presentation/ui/home/fragments/CardAccount.kt
+++ presentation/src/main/java/com/paya/presentation/ui/home/fragments/CardAccount.kt
@@ -38,7 +38,6 @@ class CardAccount : Fragment() {
 	
 	
 	private lateinit var mBinding: AccountCardBinding
-	private var isDataSet = false
 	
 	override fun onCreateView(
 		inflater: LayoutInflater,container: ViewGroup?,
@@ -52,9 +51,6 @@ class CardAccount : Fragment() {
 	}
 	
 	fun setData(linearChartRepoModel: LinearChartRepoModel,buyValue: Long,percent: Float, name: String) {
-		if (isDataSet)
-			return
-		isDataSet = true
 		mBinding.accountUserName.text = name
 		mBinding.wealthValue.text = Utils.separatorAmount(buyValue.toString())
 		
