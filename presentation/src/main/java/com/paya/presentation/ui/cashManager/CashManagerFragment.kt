05ab8528b2c043af2d0ed89cf4a413a08845a682
Arsalan <Arsalan.nasiri2013@gmail.com>
2020-12-12 22:04:44 +0330

    add cash manger

---
+++ presentation/src/main/java/com/paya/presentation/ui/cashManager/CashManagerFragment.kt
@@ -0,0 +1,62 @@
+package com.paya.presentation.ui.cashManager
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.paya.presentation.R
+import com.paya.presentation.databinding.FragmentCashManagerBinding
+import com.warkiz.widget.IndicatorSeekBar
+import com.warkiz.widget.OnSeekChangeListener
+import com.warkiz.widget.SeekParams
+import kotlinx.android.synthetic.main.fragment_calculate_profit_capital.*
+
+
+class CashManagerFragment : Fragment() {
+
+
+    private lateinit var mBinding : FragmentCashManagerBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        mBinding =  DataBindingUtil.inflate(inflater,R.layout.fragment_cash_manager, container, false)
+        mBinding.lifecycleOwner = this
+        return mBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupSeekBar()
+    }
+
+
+    private fun setupSeekBar() {
+        seekBarPrice.onSeekChangeListener = object : OnSeekChangeListener {
+            override fun onSeeking(seekParams: SeekParams?) {
+                if (seekParams != null) {
+                    inputPrice.setText(seekParams.progress.toString())
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: IndicatorSeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: IndicatorSeekBar?) {
+
+            }
+
+        }
+    }
+
+
+}
