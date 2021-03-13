2f68588bf97b84e39960cac672ced03f2b71db3a
Arsalan <Arsalan.nasiri2013@gmail.com>
2021-03-13 13:19:58 +0330

    ActivateForgotPasswordFragment.kt

---
+++ presentation/src/main/java/com/paya/presentation/ui/activateForgotPassword/ActivateForgotPasswordFragment.kt
@@ -0,0 +1,107 @@
+package com.paya.presentation.ui.activateForgotPassword
+
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.google.android.gms.auth.api.phone.SmsRetriever
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.android.gms.tasks.Task
+import com.paya.domain.tools.Resource
+import com.paya.domain.tools.Status
+import com.paya.presentation.R
+import com.paya.presentation.base.BaseFragment
+import com.paya.presentation.base.BaseViewModel
+import com.paya.presentation.databinding.FragmentActivateBinding
+import com.paya.presentation.utils.observe
+import com.paya.presentation.viewmodel.ActivateForgotPasswordViewModel
+import com.paya.presentation.viewmodel.ActivateViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+
+@AndroidEntryPoint
+class ActivateForgotPasswordFragment : BaseFragment<ActivateForgotPasswordViewModel>() {
+	
+	private val mViewModel: ActivateForgotPasswordViewModel by viewModels()
+	private lateinit var mBinding: FragmentActivateBinding
+	private val args by navArgs<ActivateForgotPasswordFragmentArgs>()
+	
+	override fun onCreateView(
+		inflater: LayoutInflater,container: ViewGroup?,
+		savedInstanceState: Bundle?
+	): View? {
+		// Inflate the layout for this fragment
+		mBinding = DataBindingUtil.inflate(
+			inflater,
+			R.layout.fragment_activate,
+			container,
+			false
+		)
+		
+		mViewModel.phoneNumber = args.phoneNumber
+		mBinding.viewModel = mViewModel
+		mBinding.lifecycleOwner = this
+		
+		return mBinding.root
+	}
+	
+	override fun onViewCreated(view: View,savedInstanceState: Bundle?) {
+		super.onViewCreated(view,savedInstanceState)
+		observe(mViewModel.status, ::checkActivateStatus)
+		mBinding.changeNumber.setOnClickListener {
+			findNavController().popBackStack()
+		}
+		requestHint()
+	}
+	
+	private fun checkActivateStatus(activateResource: Resource<Any>){
+		if (activateResource.status == Status.SUCCESS){
+			val bundle = Bundle()
+			bundle.putString("title", args.title)
+			findNavController().navigate(
+				R.id.navigateToSetPasswordFragment,
+				bundle
+			)
+		}else if (activateResource.status == Status.ERROR){
+			Toast.makeText(
+				requireContext(), activateResource.message, Toast.LENGTH_SHORT
+			).show()
+		}
+	}
+	
+	override val baseViewModel: BaseViewModel
+		get() = mViewModel
+
+	private fun requestHint() {
+
+		val client = context?.let {
+			SmsRetriever.getClient(it /* context */) }
+
+		val task: Task<Void> = client!!.startSmsRetriever()
+
+		task.addOnSuccessListener(OnSuccessListener<Void?> {
+			Log.d("","")
+			// Successfully started retriever, expect broadcast intent
+			// ...
+		})
+
+		task.addOnFailureListener(OnFailureListener {
+			Log.d("","")
+			// Failed to start retriever, inspect Exception for more details
+			// ...
+		})
+
+	}
+
+
+
+
+}
---
+++ presentation/src/main/java/com/paya/presentation/viewmodel/ActivateForgotPasswordViewModel.kt
@@ -0,0 +1,92 @@
+package com.paya.presentation.viewmodel
+
+import androidx.databinding.ObservableField
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.paya.domain.models.repo.ActivateRepoModel
+import com.paya.domain.models.repo.RegisterRepoModel
+import com.paya.domain.tools.Resource
+import com.paya.domain.tools.Status
+import com.paya.domain.tools.UseCase
+import com.paya.presentation.base.BaseViewModel
+import com.paya.presentation.utils.VolatileLiveData
+import com.paya.presentation.utils.callResource
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class ActivateForgotPasswordViewModel @ViewModelInject constructor(
+	private val activateUseCase: UseCase<ActivateRepoModel,Any>,
+	private val registerUseCase: UseCase<String,RegisterRepoModel>
+) : BaseViewModel() {
+	
+	val title = MutableLiveData<String>()
+	val remainingTimeText = MutableLiveData<String>()
+	val remainingTime = MutableLiveData(59)
+	
+	init {
+		setRemainingTime()
+	}
+	
+	lateinit var phoneNumber: String
+	val activationCode = ObservableField<String>()
+	
+	val status = VolatileLiveData<Resource<Any>>()
+	
+	fun setTitle(title: String) {
+		this.title.value = title
+	}
+	
+	fun activate() {
+		val activationCode = activationCode.get()
+		if (activationCode.isNullOrBlank() || activationCode.length != 5) {
+			status.setValue(Resource.error("code is not valid",null))
+			return
+		}
+		viewModelScope.launch(Dispatchers.IO) {
+			status.postValue(Resource.loading(null))
+			val activateModel = ActivateRepoModel(
+				phoneNumber,
+				activationCode
+			)
+			val response = callResource(this@ActivateForgotPasswordViewModel,activateUseCase.action(activateModel))
+			status.postValue(response)
+		}
+	}
+	
+	fun register() {
+		if (remainingTime.value != 0)
+			return
+		viewModelScope.launch(Dispatchers.IO) {
+			status.postValue(Resource.loading(null))
+			val response = callResource(this@ActivateForgotPasswordViewModel,registerUseCase.action(phoneNumber))
+			
+			if (response.status == Status.SUCCESS) {
+				status.postValue(Resource.idle(null))
+				remainingTime.postValue(59)
+				setRemainingTime()
+			} else
+				status.postValue(response)
+		}
+	}
+	
+	
+	private fun setRemainingTime() {
+		viewModelScope.launch {
+			val formattedRemainingTime =
+				if (remainingTime.value!! >= 10) remainingTime.value.toString()
+				else "0${remainingTime.value}"
+			remainingTimeText.value = "00:$formattedRemainingTime"
+			
+			if (remainingTime.value == 0)
+				return@launch
+			
+			delay(1000)
+			remainingTime.value = remainingTime.value!! - 1
+			
+			setRemainingTime()
+		}
+	}
+	
+}
