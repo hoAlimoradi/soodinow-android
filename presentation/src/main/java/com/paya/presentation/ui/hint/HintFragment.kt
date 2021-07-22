011e9d759c87b9d98649f899cda0d0a923288ca8
Arsalan <Arsalan.nasiri2013@gmail.com>
2021-07-22 23:30:39 +0430

    fix otp call

--- presentation/src/main/java/com/paya/presentation/ui/hint/HintFragment.kt
+++ presentation/src/main/java/com/paya/presentation/ui/hint/HintFragment.kt
@@ -1,15 +1,21 @@
 package com.paya.presentation.ui.hint
 
+import android.content.IntentFilter
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.google.android.gms.auth.api.phone.SmsRetriever
+import com.google.android.gms.tasks.Task
 import com.paya.presentation.R
 import com.paya.presentation.base.BaseFragment
 import com.paya.presentation.base.BaseViewModel
 import com.paya.presentation.databinding.FragmentHintBinding
+import com.paya.presentation.utils.AppSignatureHelper
+import com.paya.presentation.utils.MySMSBroadcastReceiver
 import com.paya.presentation.utils.getVersionName
 import com.paya.presentation.viewmodel.HintViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -19,7 +25,7 @@ class HintFragment : BaseFragment<HintViewModel>() {
 
 	private var mBinding: FragmentHintBinding? = null
 	private val mViewModel: HintViewModel by viewModels()
-
+	private var registerSms: MySMSBroadcastReceiver? = null
 	override fun onCreateView(
 		inflater: LayoutInflater, container: ViewGroup?,
 		savedInstanceState: Bundle?
@@ -33,8 +39,8 @@ class HintFragment : BaseFragment<HintViewModel>() {
 
 		return mBinding?.root
 	}
-	
-	override fun onViewCreated(view: View,savedInstanceState: Bundle?) {
+
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 		super.onViewCreated(view, savedInstanceState)
 		mBinding?.apply {
 			register.setOnClickListener {
@@ -45,7 +51,24 @@ class HintFragment : BaseFragment<HintViewModel>() {
 			}
 			versionTxt.text = "  نسخه  ${context?.let { getVersionName(it) }}"
 		}
+		/*val appS = AppSignatureHelper(requireContext().applicationContext)
+		Log.d("appSignatures", appS.appSignatures.toString())
+		Log.d("appSignaturesHash", appS.hashCode().toString())*/
+	}
 
+	override fun onResume() {
+		super.onResume()
+		//requestHint()
+	}
+
+	override fun onPause() {
+		super.onPause()
+		activity?.let { activity ->
+			if (registerSms != null) {
+				activity.unregisterReceiver(registerSms)
+				registerSms = null
+			}
+		}
 	}
 
 	override fun onDestroyView() {
@@ -53,8 +76,47 @@ class HintFragment : BaseFragment<HintViewModel>() {
 		super.onDestroyView()
 	}
 
+	/*private fun requestHint() {
+
+		val client = context?.let {
+			SmsRetriever.getClient(it)
+		}
+
+		client?.let { client ->
+			val task: Task<Void> = client.startSmsRetriever()
+
+			task.addOnSuccessListener {
+				Log.d("", "")
+				// Successfully started retriever, expect broadcast intent
+				// ...
+				registerSms = MySMSBroadcastReceiver()
+				registerSms?.initOTPListener(object : MySMSBroadcastReceiver.OTPReceiveListener {
+					override fun onOTPReceived(otp: String) {
+						Log.d("OTPReceiveListener", otp)
+					}
+
+					override fun onOTPTimeOut() {
+
+					}
+
+				})
+				activity?.let { activity ->
+					val filter = IntentFilter(SmsRetriever.SMS_RETRIEVED_ACTION)
+					activity.registerReceiver(registerSms, filter)
+
+				}
+			}
+
+			task.addOnFailureListener{
+				Log.d("", "")
+				// Failed to start retriever, inspect Exception for more details
+				// ...
+			}
+		}
+
+	}*/
 
 	override val baseViewModel: BaseViewModel
 		get() = mViewModel
-	
+
 }
--- presentation/src/main/java/com/paya/presentation/utils/MySMSBroadcastReceiver.kt
+++ presentation/src/main/java/com/paya/presentation/utils/MySMSBroadcastReceiver.kt
@@ -3,25 +3,48 @@ package com.paya.presentation.utils
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 import com.google.android.gms.auth.api.phone.SmsRetriever
 import com.google.android.gms.common.api.CommonStatusCodes
 import com.google.android.gms.common.api.Status
 
 
 class MySMSBroadcastReceiver : BroadcastReceiver() {
+    private var otpReceiver: OTPReceiveListener? = null
+
+    fun initOTPListener(receiver: OTPReceiveListener) {
+        this.otpReceiver = receiver
+    }
     override fun onReceive(context: Context?, intent: Intent) {
         if (SmsRetriever.SMS_RETRIEVED_ACTION == intent.action) {
             val extras = intent.extras
             val status: Status? = extras!![SmsRetriever.EXTRA_STATUS] as Status?
             if (status != null) {
-                when (status.getStatusCode()) {
+                when (status.statusCode) {
                     CommonStatusCodes.SUCCESS -> {       // Get SMS message contents
-                        var message: String? = extras[SmsRetriever.EXTRA_SMS_MESSAGE] as String?
+                        // Get SMS message contents
+                        var otp: String = extras.get(SmsRetriever.EXTRA_SMS_MESSAGE) as String
+                        Log.d("OTP_Message", otp)
+                        // Extract one-time code from the message and complete verification
+                        // by sending the code back to your server for SMS authenticity.
+                        // But here we are just passing it to MainActivity
+                        if (otpReceiver != null) {
+                            otp = otp.replace("<#> کد فعال سازی سودینو:", "").split("\n".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()[0]
+                            otpReceiver!!.onOTPReceived(otp)
+                        }
                     }
                     CommonStatusCodes.TIMEOUT -> {
+                        otpReceiver!!.onOTPTimeOut()
                     }
                 }
             }
         }
+    }
+
+    interface OTPReceiveListener {
+
+        fun onOTPReceived(otp: String)
+
+        fun onOTPTimeOut()
     }
 }
